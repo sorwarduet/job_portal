5d653968acbdfa6790ae7bf9af4f0f14e3b2f252
Md. Sorwar Alam <esorwar.cse@gmail.com>
2020-11-22 21:54:16 +0600

    add user register form and view

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
 
+app_name = 'users'
+
 
 urlpatterns = [
- 
+    path('register/', UserRegisterView.as_view(), name='register')
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import CreateView
+
 
 # Create your views here.
+from .form import AccountRegisterForm
+from .models import Account
+
+class UserRegisterView(SuccessMessageMixin, CreateView):
+    template_name = 'users/user-register.html'
+    form_class = Account
+    success_url = '/'
+    success_message = "Your user account has been created"
+    
